6d03342a7ca5e505fa87c6a6822f1db875086814
zachdaniels98 <zachdaniels98@gmail.com>
2021-01-20 17:50:15 -0800

    Completed UI Layout for player info

--- zsda/static/js/baseball.player.js
+++ zsda/static/js/baseball.player.js
@@ -2,6 +2,74 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../scss/custom.scss';
 
+class PlayerInfo extends React.Component {
+    render() {
+        return (
+            <div className="card text-center">
+                <div className="card-body">
+                    <h4 className="card-title">Player Name</h4>
+                    <div className="btn-group" role="group" aria-label="pitch type checkbox toggle button group">
+                        <input type="checkbox" className="btn-check" id="btncheck1" autoComplete="off"></input>
+                        <label className="btn btn-outline-primary" htmlFor="btncheck1">4-Seam Fastball</label>
+                        <input type="checkbox" className="btn-check" id="btncheck2" autoComplete="off"></input>
+                        <label className="btn btn-outline-primary" htmlFor="btncheck2">Slider</label>
+                        <input type="checkbox" className="btn-check" id="btncheck3" autoComplete="off"></input>
+                        <label className="btn btn-outline-primary" htmlFor="btncheck3">Curve</label>
+                        <input type="checkbox" className="btn-check" id="btncheck4" autoComplete="off"></input>
+                        <label className="btn btn-outline-primary" htmlFor="btncheck4">Changeup</label>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <table className="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">W-L</th>
+                                        <td>8-4</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">ERA</th>
+                                        <td>2.45</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">G</th>
+                                        <td>21</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">GS</th>
+                                        <td>21</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="col">
+                            <table className="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">IP</th>
+                                        <td>86.2</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">H</th>
+                                        <td>124</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">R</th>
+                                        <td>32</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">ER</th>
+                                        <td>27</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 class ZoneSquare extends React.Component {
     render() {
         return (
@@ -24,10 +92,9 @@ class Zone extends React.Component {
         for (let i = 0; i < 3; i++) {
             let row = []
             for (let j = 0; j < 3; j++) {
-                row.push(<ZoneSquare />);
+                row.push(<ZoneSquare key={(3 * i) + (1 + j)}/>);
             }
-            console.log(row);
-            zone.push(<div className="zone-row">{row}</div>);
+            zone.push(<div className="zone-row" key={i}>{row}</div>);
         }
         return zone;
     }
@@ -51,3 +118,8 @@ ReactDOM.render(
     </div>,
     document.getElementById('strikezone')
 );
+
+ReactDOM.render(
+    <PlayerInfo />,
+    document.getElementById('player-info')
+)
--- zsda/static/webpack.config.js
+++ zsda/static/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader",
                 options: { 
-                    presets: ["@babel/env"] 
+                    presets: ["@babel/preset-env"] 
                 }
             },
             {
